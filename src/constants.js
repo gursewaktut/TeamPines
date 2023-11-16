7e946f81d91e90dd4b6d01742770d41747af19f2
Gursewak  Singh <gursewaktut@gmail.com>
2023-11-16 08:48:41 -0800

    issue #11 implemented part of codingchallenges features

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const HACKEREARTH_URL = 'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/';
+export const STEAMSHIP_API_URL = 'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/';
+export const HACKER_EARTH_KEY =  "344232da6dd0487ee14997abf1eef9306564a654";
--- src/pages/CodingChallenge.js
+++ src/pages/CodingChallenge.js
@@ -1,17 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, Text, useDisclosure } from '@chakra-ui/react';
+import { Flex, Box, Button, Text, useDisclosure } from '@chakra-ui/react';
 import CodeEditor from '../components/CodeEditor';
 import Steamship from "@steamship/client"
 import { fetchQuestion, checkAnswer } from '../api/steamShip_client'; // Mock functions to represent API calls.
+import { HACKEREARTH_URL as hackerEarthUrl, HACKER_EARTH_KEY as hackerearthKey } from '../constants.js'
 
 const CodingChallenge = () => {
-  const [code, setCode] = useState('// Type your code here');
+  const [code, setCode] = useState('# Type your code here');
+  const [output, setOutput] = useState({});
   const [language, setLanguage] = useState('python');
   const [theme, setTheme] = useState('vs-dark');
   const [question, setQuestion] = useState({});
   const { isOpen: isAnswerOpen, onToggle: onToggleAnswer } = useDisclosure();
   const { isOpen: isExplanationOpen, onToggle: onToggleExplanation } = useDisclosure();
+  const {checkOutput, setCheckOutput} = useState('false');
 
+  const comments = {python: "#Type your code here", javascript: "//Type your code here", html: "<!-- Type your code here -->" }
+
+
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.set("Content-Type", "application/json");
+  requestHeaders.set("client-secret", `${hackerearthKey}`);
+
+
+//  const requestBody = JSON.stringify({
+//    "lang": `${language}`,
+//    "source": `${code}`,
+//    "input": "",
+//    "memory_limit": 243232,
+//    "time_limit": 5,
+//    "context": {id: 213121},
+//    //"callback": "https://client.com/callback/"
+//  });
   useEffect(() => {
     async function loadQuestion() {
       const questionData = await fetchQuestion(); // Fetch the initial question
@@ -21,8 +41,23 @@ const CodingChallenge = () => {
     loadQuestion();
   }, []);
 
+  useEffect((checkOutput) => {
+    function getOutput() {
+      if (checkOutput) {
+        console.log('output called');
+      }
+    }
+    getOutput()
+    const interval = setInterval(() => getOutput(), 3000)
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
+
   const handleLanguageChange = (event) => {
     setLanguage(event.target.value);
+    setCode(comments[event.target.value]);
   };
 
   const handleThemeChange = (event) => {
@@ -34,21 +69,47 @@ const CodingChallenge = () => {
   };
 
   const checkCode = async () => {
-    const result = await checkAnswer(code, language); // Assume checkAnswer also needs the language
+    //const apiKey = process.env.API_KEY;
+    //console.log(apiKey);
+    //const result = await checkAnswer(code, language); // Assume checkAnswer also needs the language
     // Handle the result of the code check here
+    const result = await fetch(hackerEarthUrl, {
+      method: 'POST',
+      headers: requestHeaders,
+      body:JSON.stringify({
+        "lang": `${language}`,
+        "source": `${code}`,
+        "input": "",
+        "memory_limit": 243232,
+        "time_limit": 5,
+        "context": {id: 213121},
+        //"callback": "https://client.com/callback/"
+      })
+    });
+
+    setOutput(result.json());
+    //.then(result => setOutput(result.compile_status))
   };
 
   return (
     <Box p={4}>
       <Text mb={4}>{question.text || 'Loading question...'}</Text>
-      <CodeEditor
-        code={code}
-        onChange={handleChange}
-        language={language}
-        theme={theme}
-        handleLanguageChange={handleLanguageChange}
-        handleThemeChange={handleThemeChange}
-      />
+      <Flex >
+        <Box p={4} w={"50%"}>
+          <CodeEditor
+            code={code}
+            onChange={handleChange}
+            language={language}
+            theme={theme}
+            handleLanguageChange={handleLanguageChange}
+            handleThemeChange={handleThemeChange}
+          />
+        </Box>
+
+        <Box p={4} w={"50%"}>
+          <Text>{output.result || 'Ouput..'}</Text>
+        </Box>
+      </Flex>
       <Box display="flex" mt={4}>
         <Button onClick={checkCode} colorScheme="blue">Check Code</Button>
         <Button onClick={onToggleAnswer} ml={4}>Answer</Button>
