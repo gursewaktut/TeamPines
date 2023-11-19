e454b949c390b2250f7634b446d896085e5fc01f
Gursewak  Singh <gursewaktut@gmail.com>
2023-11-19 09:10:50 -0800

    moved the tests and added some more tests

---
+++ src/__tests__/e2e/integrationTest.test.js
@@ -0,0 +1,25 @@
+const webdriver = require("selenium-webdriver");
+const By = webdriver.By; // useful Locator utility to describe a query for a WebElement
+const driver = new webdriver.Builder().forBrowser("firefox").build();
+const actions = driver.actions({bridge: true});
+// Instantiate a web browser page
+describe('LanguageSelection', () => {
+  it('checks if the language selected is the same as language being used', async () => {
+    await driver.navigate().to("localhost:3000")
+      .then(() => driver.findElement(By.id("getstarted")))
+      .then(element => element.click())
+      .then(() => driver.findElement(By.id("python")))
+      .then(element => element.click())
+      .then(() => driver.findElement(By.id("languages")))
+      .then(element => element.getAttribute("value"))
+      .then(value => console.log(value))
+      .then(() => driver.findElement(By.id("languages")))
+      .then(element => element.getAttribute('value'))
+      .then(value => {
+        console.log(value);
+        expect(value).toBe('python');
+        driver.quit();
+      });
+  });
+  //.then(driver.quit());
+});
--- src/__tests__/unit/CodingChallenge.test.js
+++ src/__tests__/unit/CodingChallenge.test.js
@@ -1,7 +1,7 @@
 // CodingChallenge.test.js
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import CodingChallenge from '../../src/pages/CodingChallenge'; // Adjust the import path as necessary
+import CodingChallenge from '../../pages/CodingChallenge'; // Adjust the import path as necessary
 
 describe('CodingChallenge', () => {
   it('renders the component and finds the Check Code button', () => {
@@ -13,6 +13,7 @@ describe('CodingChallenge', () => {
   });
 
   it('calls checkCode function when the Check Code button is clicked', () => {
+
     const mockCheckCode = jest.fn();
     CodingChallenge.prototype.checkCode = mockCheckCode; // Mock the method
 
@@ -23,4 +24,20 @@ describe('CodingChallenge', () => {
     // Expect the mock function to have been called
     expect(mockCheckCode).toHaveBeenCalled();
   });
+
+  it('renders the component and finds the Answer button', () => {
+    const { getByText } = render(<CodingChallenge />);
+
+    // Check if the button text is in the document
+    const answerButton = getByText('Answer');
+    expect(answerButton).toBeInTheDocument();
+  });
+
+  it('calls answer function when the Visual Explanation button is clicked', () => {
+    const { getByText } = render(<CodingChallenge />);
+
+    // Check if the button text is in the document
+    const vsButton = getByText('Visual Explanation');
+    expect(vsButton).toBeInTheDocument();
+  })
 });
