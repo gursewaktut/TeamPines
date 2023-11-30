25c055e04a98dcb55966d7ad040f10263bfb846d
HENRIK <hsa206@sfu.ca>
2023-11-30 00:34:32 -0800

    Added two more unit tests in CodingChallenge.test.js

--- src/__tests__/unit/CodingChallenge.test.js
+++ src/__tests__/unit/CodingChallenge.test.js
@@ -1,6 +1,6 @@
 // CodingChallenge.test.js
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import CodingChallenge from '../../pages/CodingChallenge'; // Adjust the import path as necessary
 
 
@@ -26,3 +26,30 @@ describe('CodingChallenge', () => {
   });
 });
 
+describe('CodingChallenge', () => {
+  it('Checks if Tutor mode comes off', async () => {
+    render(<CodingChallenge />);
+
+    const tutorModeButton = screen.getByText(/Tutor Mode/i);
+    fireEvent.click(tutorModeButton); //Click once to turn on
+    fireEvent.click(tutorModeButton); //Click twice to turn off
+
+    await waitFor(() => {
+      expect(screen.getByText(/Tutor Mode(?! ON)/i)).toBeInTheDocument(); // Update the text to match the content of the Answer section
+    });
+  });
+});
+
+describe('CodingChallenge', () => {
+  it('Opens the answer on clicking the Answer button', async () => {
+    render(<CodingChallenge />);
+
+    const answerButton = screen.getByText(/Answer/i);
+
+    fireEvent.click(answerButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Given the head of a singly linked list/i)).toBeInTheDocument(); // Update the text to match the content of the Answer section
+    });
+  });
+});
